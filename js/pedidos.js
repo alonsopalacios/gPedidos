f8a9ffe216cb4fe0468cad5369d398d111759991
PORTATIL7\Usuario <alonso_ben.2009@hotmail.com>
2017-02-27 11:23:33 +0100

    cambios .ap

--- js/pedidos.js
+++ js/pedidos.js
@@ -24,26 +24,41 @@ function Pedido(fechaPedido, totalArticulos, totalPedido, direccionEntrega, dire
 	this.getFechaPedido = function(){
 		return this.fechaPedido;
 	};
+	//Meodo para cambiar la fecha del pedido
+	this.setfechaPedido = function (){
+		this.fechaPedido = fechaPedido;
+	};
 
 	//Metodo para conocer el total de articulos del pedido
 	this.gettotalArticulos = function(){
 		return this.totalArticulos;
 	};
+	//Metodo para cambiar el total de articulos del pedido
+	this.settotalArticulos = function(){
+		this.totalArticulos = totalArticulos
+	};
 		//Metodo para conocer el total de pedidos
 	this.gettotalPedido = function(){
 		return this.totalPedido;
+	};
+	//Metodo para cambiar el total de pedidos
+	this.settotalPedido = function(){
+		this.totalPedido = totalPedido
 	};
 		//Metodo para conocer el direccion de Entrega del Pedido
 	this.getdireccioEntrega = function(){
 		return this.direccioentrega;
 	};
-			//Metodo para conocer la direccion de facturación del Pedido
-	this.gettotalPedido = function(){
-		return this.direccionFacturación;
-	};
+	//Metodo para cambiar la direccion de entrega del pedido
 	this.setdireccionEntrega =function(direccionEntrega){
 		this.direccionEntrega=direccionEntrega
 	};
+	
+			//Metodo para conocer la direccion de facturación del Pedido
+	this.getdirecciofactración= function(){
+		return this.direccionFacturación;
+	};
+	//Metodo para cambiar la direccion de facturación del pedido
 	this.setdireccionFacturacion = function (direccionFacturación){
 		this.direccionFacturación = this.direccionFacturación
 	};
